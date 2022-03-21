f63a47c0adb043a8e9dae8bdeb1b7c518cfa6156
Janice <72073506+janicelie@users.noreply.github.com>
2022-03-21 14:37:35 +0700

    added projects section

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 const toTopBtn = document.getElementById("toTop");
 const navbar = document.getElementById("navbar");
+const about = document.getElementById("about");
+const projectSection = document.getElementById("projects-container");
+
+var projectData = [
+  {
+    name: "KryptMe",
+    desc: "File encryption software with 2FA.",
+    img: "img/janice-photo.jpg",
+    link: "https://google.com",
+  },
+  {
+    name: "ToDos",
+    desc: "Simple To-Dos List that allows you to track your activities for a day!",
+    img: "img/janice-photo.jpg",
+    link: "https://google.com",
+  },
+  {
+    name: "Movie App",
+    desc: "Movie lovers? Use this app to search for the latest and most popular movie!",
+    img: "img/janice-photo.jpg",
+    link: "https://google.com",
+  },
+  {
+    name: "Food Recipe App",
+    desc: "Love cooking but don't know the recipe? Come find your favourite meals and cook for your loved one!",
+    img: "img/janice-photo.jpg",
+    link: "https://google.com",
+  },
+];
+
+showProject(projectData);
+
+function showProject(projectData) {
+  projectData.forEach((project) => {
+    const { name, desc, img, link } = project;
+    const projectEl = document.createElement("div");
+    projectEl.classList.add("project");
+    projectEl.innerHTML = `
+      <img src="${img}"/>
+      <div class="project-body">
+          <h4>${name}</h4>
+          <p>${desc}</p>
+          <a class="button" href="${link}" target="_blank">View</a>
+      </div>
+  `;
+    projectSection.appendChild(projectEl);
+  });
+}
 
 window.onscroll = function () {
   scrollFunction();
